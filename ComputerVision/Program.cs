4bedcafddc5cfd6cbfa73f9f2c6eb15abb79dd2f
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2022-04-09 18:06:57 -0300

    integração com o cognitive service - analise de imagem

---
+++ ComputerVision/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+Console.WriteLine("Hello, World!");
+
+string Key = "minha chave privada";
+string endpoint = "url do meu serviço de cognitive service da azure";
+string Url = "https://cdn.pixabay.com/photo/2015/08/12/10/20/person-885698_960_720.jpg";
+
+ComputerVisionClient computerVision = new ComputerVisionClient(new ApiKeyServiceClientCredentials(Key)) 
+ { Endpoint = endpoint } ;
+
+var analises = computerVision.DescribeImageAsync(Url, 10, "pt").GetAwaiter().GetResult();
+
+foreach (var item in analises.Captions)
+{
+    Console.WriteLine($"{item.Text}, {item.Confidence}");
+}
