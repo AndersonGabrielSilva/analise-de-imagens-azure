fca7e61d8682f19c0ea90987186a21097060cc29
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2022-04-09 17:36:05 -0300

    Azure function - trigger blob Disparado sempre que um novo arquivo é adicionado no container

---
+++ ImagemAdicionada/Function1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ImagemAdicionada
+{
+    public class Function1
+    {
+
+        [FunctionName("upload-image")]
+        [StorageAccount("AzureWebJobsStorage")]
+        public void Run([BlobTrigger("analise-imagens/{name}", Connection = "AzureWebJobsStorage")]BlobClient blobCLient, string name, ILogger log)
+        {
+            log.LogInformation($"Blob adicionado \n Name:{name} \n");
+         
+            log.LogInformation("URL: " + blobCLient.Uri.AbsoluteUri);
+
+
+        }
+    }
+}
